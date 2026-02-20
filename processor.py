dac6253d0f3d3782f4e33e9ee5246c1be55d3c2f
Matthew Flanagan <matthew.flanagan>
2026-02-20 16:42:05 -0500

    mostly psuedo code currently

--- processor.py
+++ processor.py
@@ -1,2 +1,14 @@
 #takes timestamps + labels and decides where clips start and end
 #calls clipper.py to generate all clips
+
+from clipper import cut_clip
+from pathlib import Path
+import json
+
+def process_video(video_path: str, events_file: str):
+    """
+    video_path locates the video file
+    events_file JSON file containing timestamps
+    """
+    print("Starting video processing now....")
+
