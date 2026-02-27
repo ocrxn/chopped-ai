8e89c9d0b80f48b75d3b2a4a7c88c6b499ca8831
Matthew Flanagan <matthew.flanagan>
2026-02-27 16:53:58 -0500

    updates .json file opening and reading

--- processor.py
+++ processor.py
@@ -4,8 +4,37 @@
 from clipper import cut_clip
 from pathlib import Path
 import json
+import os
+
+#function to load events from .json file
+def load_events(events_file):
+
+    events_file = Path(events_file)
+
+    if not events_file.exists:
+        raise Exception("file does not exist")
+
+    # Open JSON file with exceptions
+    try:
+        with open(events_file, 'r') as file:
+            # load JSON data
+            data = json.load(file)
+    except json.JSONDecodeError:
+            raise Exception("Events JSON is not valid JSON")
+            
+    # check to make sure it's a list
+    if not isinstance(data, list):
+        raise Exception("Events JSON must be a list")
+    
+    return data
+        
+
 
 def process_video(video_path: str, events_file: str):
+    video_path = Path(video_path)
+
+    events = load_events(events_file)
+
     """
     video_path locates the video file
     events_file JSON file containing timestamps
