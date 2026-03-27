f3a67003f2f70f9c9e0306becf73c8db3f69ba30
Matthew Flanagan <matthew.flanagan>
2026-03-27 15:58:08 -0400

    working prototype

--- clipper.py
+++ clipper.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
-import subprocess #lets Python run other programs on computer (FFmpeg)
+import subprocess  # lets Python run other programs on computer (FFmpeg)
 from pathlib import Path
 import json
 import os
-
 
 
 def clip_video(video_path: str, output_path: str, start_time: int, duration: int = 35):
@@ -12,22 +11,21 @@ def clip_video(video_path: str, output_path: str, start_time: int, duration: int
     output_path = os.path.abspath(output_path)
 
     if not os.path.exists(video_path):
-        raise FileNotFoundError("input video not found: {video_path}")
+        raise FileNotFoundError(f"input video not found: {video_path}")
+
     command = [
         "ffmpeg",
-        "-ss", str(start_time), #start cutting at start time
-        "-i", video_path, #input video file
-        "-t", str(duration), #duration of the cut
-        "-c", "copy", #copies audio and video streams without re-encoding
-                      #makes process faster
-        "-y", #so ffmpeg won't stop if file already exists
-        output_path #output path for cut video file
+        "-ss", str(start_time),  # start cutting at start time
+        "-i", video_path,  # input video file
+        "-t", str(duration),  # duration of the cut
+        "-c", "copy",  # copies audio and video streams without re-encoding
+        "-y",  # so ffmpeg won't stop if file already exists
+        output_path,  # output path for cut video file
     ]
-
 
     subprocess.run(command, check=True)
 
-#takes in video file path
-#takes in data from JSON file from processor.py
-#uses JSON parameters
-#uses ffmpeg to cut clips and put into folders
+# takes in video file path
+# takes in data from JSON file from processor.py
+# uses JSON parameters
+# uses ffmpeg to cut clips and put into folders
